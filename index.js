9366dd310b72d790587f836e5054ba5595a5176e
Darkot1 <sebarango136@gmail.com>
2024-05-24 02:01:59 -0500

    implementacion carrusel y tarjetas dinamicas

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
- 
-  let btnOpenPopup = document.getElementById('btnOpenPopup');
-  let popup = document.getElementById('popup');
-  let popupContent = document.getElementById('popup-content')
-  let colors = ['#5ACCA4', '#EAA2FC', '#FE654F'];
-  // Evento de clic para abrir el popup
-  btnOpenPopup.addEventListener('click', function() {
-    popup.style.display = 'block';
+const btnOpenPopups = document.querySelectorAll('.btnOpenPopup');
+const popup = document.getElementById('popup');
+const popupContent = document.getElementById('popup-content');
 
-    let randomColor = colors[Math.floor(Math.random() * colors.length)];
-    popupContent.style.backgroundColor = randomColor;
-  });
+// recorrer cada targeta para que se puedo abrir el popup en cada una
+btnOpenPopups.forEach(btn => {
+  btn.addEventListener('click', () => {
+    popup.style.display = 'block'; 
+   });
+});
 
-  // Cerrar el popup 
-  window.addEventListener('click', function(event) {
-    if (event.target == popup) {
-      popup.style.display = 'none';
+// Cerrar el popup
+window.addEventListener('click', (e) => {
+    if (e.target == popup) {
+        popup.style.display = 'none';
     }
-  });
+});
 
-  //popup Wall 
-  let btnOpenPopup2 = document.getElementById('btnOpenPopup2');
-  let popup2 = document.getElementById('popup2');
-  let popupContent2 = document.getElementById('popup-content2')
-  // Evento de clic para abrir el popup
-  btnOpenPopup2.addEventListener('click', function() {
-    popup2.style.display = 'block';
 
-    let randomColor = colors[Math.floor(Math.random() * colors.length)];
-    popupContent2.style.backgroundColor = randomColor;
-  });
 
-  // Cerrar el popup 
-  window.addEventListener('click', function(event) {
-    if (event.target == popup2) {
-      popup2.style.display = 'none';
+//popup Wall 
+
+const btnOpenPopups2 = document.querySelectorAll('.btnOpenPopup2');
+const popup2 = document.getElementById('popup2');
+const popupContent2 = document.getElementById('popup-content2');
+
+// recorrer cada targeta para que se puedo abrir el popup en cada una
+btnOpenPopups2.forEach(btn => {
+  btn.addEventListener('click', () => {
+    popup2.style.display = 'block'; 
+   });
+});
+
+// Cerrar el popup
+window.addEventListener('click', (e) => {
+    if (e.target == popup2) {
+        popup2.style.display = 'none';
     }
-  });
-
+});
 
 
 //Script para cerrar sesion / eliminar datos del localstorage
